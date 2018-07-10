32351d4d28b4447d6514264869f27ca205559f7e
Vaibhav <vmdhoke@gmail.com>
2018-07-11 00:15:08 +0530

    URL redirection Issue

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,13 @@
 ShopifyApp.configure do |config|
-  config.application_name = 'My Shopify App'
-  config.api_key = 'aa4eca9ba9d2aacf911e90703cc1bd37'
-  config.secret = '7de40b52c0a75c8507b7da16d3403830'
-  config.scope = 'read_orders, read_products'
+  config.application_name = "My Shopify App"
+  config.api_key = "aa4eca9ba9d2aacf911e90703cc1bd37"
+  config.secret = "7de40b52c0a75c8507b7da16d3403830"
+  config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
 
-  # https://young-headland-58413.herokuapp.com/webhooks/order_create
   config.webhooks = [
-    { topic: 'orders/create', address: 'https://young-headland-58413.herokuapp.com/webhooks/order_create', format: 'json' }
+    { topic: "orders/create", address: "https://young-headland-58413.herokuapp.com/webhooks/order_create", format: 'json' }
   ]
 end
